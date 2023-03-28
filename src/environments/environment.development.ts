8920cad57759fed26d0e233b103702f2b70c91d1
jst4rk <dayron.alfa@gmail.com>
2023-03-27 23:14:35 -0400

    chore: update environment and fix a gateway page ui issue

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  apiUrl: 'https://gw-api.herokuapp.com',
-  // uncomment to test in a local env
-  // apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3000',
 };
